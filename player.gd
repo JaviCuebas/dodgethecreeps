64ae368e35de79616fedf5e56d4392e210f8c813
= <=>
2025-08-30 16:37:22 -0400

    first commit

---
+++ player.gd
@@ -0,0 +1,12 @@
+extends Area2D
+@export var speed = 400 # How fast a player will move (pixels/sec).
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
