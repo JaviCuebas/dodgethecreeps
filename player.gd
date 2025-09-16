197d1f8c2d88e90d994dab437cb32b0d16f973a0
JaviCuebas <javier.cuebas1@upr.edu>
2025-09-15 21:06:03 -0400

    Added inputs

--- player.gd
+++ player.gd
@@ -1,12 +1,36 @@
 extends Area2D
 @export var speed = 400 # How fast a player will move (pixels/sec).
+var screen_size
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _ready():
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _process(delta):
+	
+	# Checks for inputs being pressed.
+	
+	var velocity = Vector2.ZERO # The player's movement vector
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+		
+	# Updates the players position.
+	
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
